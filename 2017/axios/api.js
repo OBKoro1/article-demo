0163d06ac578e0462371c280df70f2171d6eb658
OBKoro1 <1677593011@qq.com>
2017-11-05 15:33:11 +0800

    axios封装请求的文件

---
+++ 2017/axios/api.js
@@ -0,0 +1,39 @@
+﻿import { fetch } from "./fetch"; //引用fetch.js
+import api from './url'; //引用url.js
+
+
+//查看用户
+export function lookOption(issuer,userId) { //lookOption是你要调用接口的名字，issuer,userId是传进来的参数
+  return fetch({
+    //api.Hallowmas 引用url.js里面的数据
+    url: api.Hallowmas+'/halloween/'+issuer+'/question',
+    method: 'get',//请求方法
+    params:{
+      currentUserId:userId //传过去的参数
+    }
+  })
+}
+
+//有新接口的时候像上面那样再来一次
+// //修改昵称接口
+// export function userID(name){
+//   return fetch({
+//     url:api.myself_name,
+//     method:"put",
+//     data:{
+//       nickname:name
+//     }
+//   })
+// }
+//
+//
+// //取消转发赞踩接口
+// export function cancelForward(articleId,type){
+//   return fetch({
+//     url:api.detail_article+articleId+"/forwarded_impress",
+//     method:"delete",
+//     params:{
+//       type:type
+//     }
+//   })
+// }
